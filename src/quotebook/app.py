3085af1bd79559fcfb938ae1b7b2f7143638b033
BD103 <59022059+BD103@users.noreply.github.com>
2022-01-18 17:51:06 +0000

    feat: add footer and version number

--- src/quotebook/app.py
+++ src/quotebook/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from quotebook.bridge import compress, configuration, csrf, db, login_manager
 from quotebook.core.bp import bp as core_bp
+from quotebook.__version__ import __version__
 
 
 def create_app(name: str) -> Flask:
@@ -17,5 +18,9 @@ def create_app(name: str) -> Flask:
 
     with app.app_context():
         db.create_all()
+    
+    @app.context_processor
+    def inject_version():
+        return dict(version=__version__)
 
     return app
