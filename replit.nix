92b653d5cab3abd63e23a3d4df6a753322cffd74
BD103 <59022059+BD103@users.noreply.github.com>
2022-03-30 17:22:55 +0000

    chore: support binary modules in replit

--- replit.nix
+++ replit.nix
@@ -2,7 +2,18 @@
     deps = [
         pkgs.python39Full
         pkgs.poetry
-        pkgs.nodePackages.prettier
+
         pkgs.vim
+        pkgs.nodePackages.prettier
     ];
+
+    env = {
+        PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc.lib
+            pkgs.zlib
+            pkgs.glib
+        ];
+
+        LANG = "en_US.UTF-8";
+    };
 }
